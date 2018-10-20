89d1ffa529ac0a46793d9a19f4c8122223212c6a
Shaggy <33808216+adamstajszczak@users.noreply.github.com>
2018-10-20 17:56:20 +0200

    init character and movement script part.1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calculate movmenet velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        //Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        //Apply movement
+        motor.Move(_velocity);
+
+    }
+}
