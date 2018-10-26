080aa0468beae2129a3e35ab70a6d8d59e78175e
Shaggy <adam.stajszczak@gmail.com>
2018-10-26 21:28:21 +0200

    poruszanie się postaci + kamera
    postać może chodzić, oprót myszy powoduje zmianę keirunku, oraz kierunek kamery

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
 
     private PlayerMotor motor;
 
@@ -15,18 +18,35 @@ public class PlayerController : MonoBehaviour {
 
     private void Update()
     {
-        //Calculate movmenet velocity as a 3D vector
+        //Oblicz kierunek poruszanai się w modelu 3D
         float _xMov = Input.GetAxisRaw("Horizontal");
         float _zMov = Input.GetAxisRaw("Vertical");
 
         Vector3 _movHorizontal = transform.right * _xMov;
         Vector3 _movVertical = transform.forward * _zMov;
 
-        //Final movement vector
+        //Ostateczny keirunek poruszania
         Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
 
-        //Apply movement
+        //Zatwierdz poruszanie sie
         motor.Move(_velocity);
+
+        //Oblicz kierunek dla vektora 3D (obrot)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Zatwierdz obrot
+        motor.Rotate(_rotation);
+
+
+        //Oblicz kierunek kamery vektora 3D (obrot)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        //Zatwierdz obrot
+        motor.RotateCamera(_cameraRotation);
 
     }
 }
--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,6 +1,62 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour {
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Pobierz vector do ruchu
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //Pobierz vektor obrotu
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    //Pobierz vektor obrotu kamery
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
 
 }
